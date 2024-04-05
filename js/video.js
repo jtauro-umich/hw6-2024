bc1dcb637ce7ecfe4de5340920aefdf8dbb4e1be
Julia Tauro <jtauro@Julias-MacBook-Pro.local>
2024-04-04 23:37:03 -0400

    autograder freebie

--- js/video.js
+++ js/video.js
@@ -1,11 +1,123 @@
-var video;
+let video;
+let playButton;
+let pauseButton;
+let slowButton;
+let fastButton;
+let skipButton;
+let muteButton;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+    console.log("Good job opening the window");
+    
+		console.log("Initializing video");
+		video = document.getElementById("player1");
+		if (video) {
+			video.autoplay = false;
+			video.loop = false;
+		}
 
-});
+		playButton = document.querySelector("#play");
+		playButton.addEventListener("click", playVid);
+	
+		pauseButton = document.querySelector("#pause");
+		pauseButton.addEventListener("click", pauseVid);
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+		slowButton = document.querySelector("#slower");
+		slowButton.addEventListener("click", slowVid);
+		
+		fastButton = document.querySelector("#faster");
+		fastButton.addEventListener("click", fastVid);
+		
+		skipButton = document.querySelector("#skip");
+		skipButton.addEventListener("click", skipVid);
 
+		muteButton = document.querySelector("#mute");
+		muteButton.addEventListener("click", toggleMute);
+
+		volumeSlider = document.querySelector("#slider");
+		volumeSlider.addEventListener("input", changeVolume);
+
+		vintageStyle = document.querySelector("#vintage");
+		vintageStyle.addEventListener("click", function() {
+			toggleStyle("oldSchool");
+		});
+
+		origStyle = document.querySelector("#orig");
+		origStyle.addEventListener("click", function() {
+			toggleStyle("");
+		});
+	});
+
+// ------------ FUNCTIONS ------------ 
+
+function playVid() {
+    console.log("Playing video");
+    if (video) {
+        video.play();
+    }
+}
+
+function pauseVid() {
+    console.log("Pausing video");
+    if (video) {
+        video.pause();
+    }
+}
+
+function slowVid() {
+	console.log("Slowing video");
+	if (video) {
+		video.playbackRate -= 0.10;
+		console.log("New speed:", video.playbackRate.toFixed(2));
+	}
+}
+
+function fastVid() {
+	console.log("Speeding up video");
+	if (video) {
+		video.playbackRate += 0.10;
+		console.log("New speed:", video.playbackRate.toFixed(2));
+	}
+}
+
+function skipVid() {
+	console.log("Skipping ahead 10 seconds");
+	if (video) {
+		if (video.currentTime + 10 >= video.duration) {
+			video.currentTime = 0;
+		} else {
+			video.currentTime += 10;
+		}
+
+		console.log("Current location of the video:", video.currentTime.toFixed(2));
+	}
+}
+
+function toggleMute() {
+	console.log("Toggling Mute");
+	if (video) {
+			video.muted = !video.muted;
+			muteButton.textContent = video.muted ? "Unmute" : "Mute";
+	}
+}
+
+function changeVolume() {
+	console.log("Changing Volume");
+	
+	if (video) {
+		video.volume = parseFloat(this.value) / 100;
+		document.getElementById("volume").textContent = this.value + "%";
+	}
+}
+
+function toggleStyle(style) {
+	if (video) {
+		if (style === "oldSchool") {
+			console.log("Implementing vintage style");
+			video.classList.add("oldSchool");
+		} else {
+			console.log("Implementing original style");
+			video.classList.remove("oldSchool")
+		}
+	}
+}
